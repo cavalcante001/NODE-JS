69e5e05779060a0f5b5eadca094b93657849701b
Paulo <paulocavalcante001@outlook.com>
2022-09-24 15:11:34 -0300

    Variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response} from 'express';
 import path from 'path';
 import mustache from 'mustache-express';
+import dotenv from 'dotenv';
 import mainRoutes from './routes/index';
 import painelRoutes from './routes/painel';
+
+dotenv.config();
 
 const server = express();
 
@@ -20,4 +23,4 @@ server.use((req: Request, res: Response) => {
     res.status(404).send('Página não encontrada!');
 });
 
-server.listen(3001);
+server.listen(process.env.PORT);
