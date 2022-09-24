c2f285544b2df176c89bdf0a654a7851602145a5
Paulo <paulocavalcante001@outlook.com>
2022-09-24 10:15:21 -0300

    Importando arquivos de terceiros - npm

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Matematica_1 = __importDefault(require("./Matematica"));
-let n1 = 10;
-let n2 = 2;
-console.log(`SOMA: ${Matematica_1.default.somar(n1, n2)}`);
-console.log(`SUBTRAÇÃO: ${Matematica_1.default.subtrair(n1, n2)}`);
-console.log(`MULTIPLICAÇÃO: ${Matematica_1.default.multiplicar(n1, n2)}`);
+const validator_1 = __importDefault(require("validator"));
+let ip = '127.0.0.1';
+console.log(validator_1.default.isEmail('loremipsum'));
+console.log(validator_1.default.isIP(ip));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import Matematica  from './Matematica';
+import validator from 'validator';
 
-let n1: number = 10;
-let n2: number = 2;
+let ip = '127.0.0.1';
 
-console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
-console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1,n2)}`);
-console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1,n2)}`);
+console.log(validator.isEmail('loremipsum'));
+console.log(validator.isIP(ip));
