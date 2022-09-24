bb4e4d19c6e76026dd56a556b2423545d7ede6ab
Paulo <paulocavalcante001@outlook.com>
2022-09-24 09:57:48 -0300

    import/export em es6

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
-const Matematica = require('./Matematica');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Matematica_1 = require("./Matematica");
 let n1 = 10;
 let n2 = 2;
-console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
-console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
-console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
+console.log(`SOMA: ${(0, Matematica_1.somar)(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${(0, Matematica_1.subtrair)(n1, n2)}`);
+console.log(`MULTIPLICAÇÃO: ${(0, Matematica_1.multiplicar)(n1, n2)}`);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const Matematica = require('./Matematica');
+import {somar, subtrair, multiplicar}  from './Matematica';
 
 let n1: number = 10;
 let n2: number = 2;
 
-console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
-console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1,n2)}`);
-console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1,n2)}`);
+console.log(`SOMA: ${somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${subtrair(n1,n2)}`);
+console.log(`MULTIPLICAÇÃO: ${multiplicar(n1,n2)}`);
