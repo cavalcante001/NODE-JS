fa23deabbb70896283300d2ffdca09614072b75a
Paulo <paulocavalcante001@outlook.com>
2022-09-24 09:50:20 -0300

    Entendo import/export em ES6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-let nome: string = "Paulo";
-let idade: number = 23;
+const Matematica = require('./Matematica');
 
-console.log(`Meu nome é ${nome} e eu tenho ${idade}`);
+let n1: number = 10;
+let n2: number = 2;
+
+console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1,n2)}`);
+console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1,n2)}`);
