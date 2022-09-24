82a51944c8cc5ab72e333c2f317076da685b49c5
Paulo <paulocavalcante001@outlook.com>
2022-09-24 11:19:55 -0300

    Criando página não encontrada - 404

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import mainRoutes from './routes/index';
 import painelRoutes from './routes/painel';
 
 const server = express();
 server.use(mainRoutes);
 server.use('/painel', painelRoutes);
+server.use((req: Request, res: Response) => {
+    res.status(404).send('Página não encontrada!');
+});
 
 server.listen(3001);
