7319e44dcaee1ab4a9e13dfa87af56d376f1978b
Paulo <paulocavalcante001@outlook.com>
2022-09-24 10:24:30 -0300

    Conhecendo o nodemon

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,10 @@ const validator_1 = __importDefault(require("validator"));
 let ip = '127.0.0.1';
 console.log(validator_1.default.isEmail('loremipsum'));
 console.log(validator_1.default.isIP(ip));
+let name = 'paulo';
+if (validator_1.default.isLowercase(name)) {
+    console.log(`A string ${name} é toda minúscula`);
+}
+else {
+    console.log(`A string ${name} não é toda minúscula`);
+}
